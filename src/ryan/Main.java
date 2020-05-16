2af7a10fb01c1b73de2751d17cc5222e0d657101
ryan ovard <rjamesovard@gmail.com>
2020-05-16 12:44:48 -0600

    Main assignment description

--- src/ryan/Main.java
+++ src/ryan/Main.java
@@ -3,9 +3,7 @@ package ryan;
 public class Main {
 
     public static void main(String[] args) {
-
         System.out.println("Hello All");
-
-        System.out.println("Hello Ryan, I'm your team mate Will.");
+        System.out.println("Hello, my name is Ryan");
     }
 }
